13e4ad968cc43f799be15a0fb30682f2addca834
luciequant <156855912+luciequant@users.noreply.github.com>
2024-05-07 12:19:11 +0200

    Added GitHub links

--- src/App.js
+++ src/App.js
@@ -2,7 +2,29 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/luciequant"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Lucie Quantová
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/luciequant/weather-react-project"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
